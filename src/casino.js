5c7571130261bdb195ed47eaae2f3cc34169c1dc
OIOpyH3e <n.raimbaev@gmail.com>
2023-12-15 01:17:55 +0600

    Добавление первого теста.

--- src/casino.js
+++ src/casino.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { engine: expressHandlebars } = require('express-handlebars')
 
-const fortune = require('./lib/fortune.js')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -16,26 +16,17 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3000
 
 // Домашняя страница
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
 
 // Страница О нас
-app.get('/about', (req, res) => { 
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
 // Пользовательская страница 404
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // Пользовательская страница 500
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express запущен на http://localhost:${port}; ` +
